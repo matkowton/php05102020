7e4583baa354a05ba46943380f5171a0a83b718e
matkowton <matkowton@gmail.com>
2020-10-08 20:51:18 +0300

    lesson 1 +  hw

--- public/hw.php
+++ public/hw.php
@@ -1,6 +1,30 @@
 <?php
+$title = 'Lesson 2';
+$content = 'functions';
+$date = date('Y-m-d');
+
+
+$a = 5;
+$b = 7;
+
+$a = $a + $b;
+$b = $a - $b;
+$a = $a - $b;
 
 ?>
-
-<h1>Title 1</h1>
-<p>content</p>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1><?= $title?></h1>
+<p><?= $content?></p>
+<p><?= $date?></p>
+    <option value=""></option>
+</body>
+</html>
